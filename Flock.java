22cf358fef7b4c93d328cde34bf2eb646ee9c277
Ellis Simmons <46097886+esimmons1@users.noreply.github.com>
2025-05-21 19:41:08 -0400

    Update Flock.java
    added comments

--- Flock.java
+++ Flock.java
@@ -2,7 +2,7 @@ ArrayList<Boid> flock = new ArrayList<Boid>();
 
 void setup() {
   size(900, 600);
-  // Create initial boids
+  // Create 100 boids at random spots
   for (int i = 0; i < 100; i++) {
     flock.add(new Boid(random(width), random(height)));
   }
@@ -11,10 +11,10 @@ void setup() {
 void draw() {
   background(220); // light gray background
   for (Boid b : flock) {
-    b.flock(flock);
-    b.update();
-    b.wrap();
-    b.display();
+    b.flock(flock);  // calculate steering forces based on neighbors
+    b.update();      // update position and velocity
+    b.wrap();        // wrap around edges
+    b.display();     // draw boid
   }
 }
 
@@ -24,33 +24,33 @@ class Boid {
 
   Boid(float x, float y) {
     pos = new PVector(x, y);
-    vel = PVector.random2D().mult(random(1, maxSpeed));
+    vel = PVector.random2D().mult(random(1, maxSpeed)); // start with random velocity
     acc = new PVector();
   }
 
   void update() {
-    vel.add(acc);
-    vel.limit(maxSpeed);
-    pos.add(vel);
-    acc.mult(0);
+    vel.add(acc);        // add acceleration to velocity
+    vel.limit(maxSpeed); // limit speed
+    pos.add(vel);        // move position
+    acc.mult(0);         // reset acceleration
   }
 
   void apply(PVector f) {
-    acc.add(f);
+    acc.add(f);          // add force to acceleration
   }
 
   void flock(ArrayList<Boid> boids) {
-    PVector sep = new PVector();
-    PVector ali = new PVector();
-    PVector coh = new PVector();
+    PVector sep = new PVector();  // separation force
+    PVector ali = new PVector();  // alignment force
+    PVector coh = new PVector();  // cohesion force
     int total = 0;
 
     for (Boid o : boids) {
       float d = PVector.dist(pos, o.pos);
       if (o != this && d < view) {
-        ali.add(o.vel);
-        coh.add(o.pos);
-        sep.add(PVector.sub(pos, o.pos).div(d * d));
+        ali.add(o.vel);                       // average velocity for alignment
+        coh.add(o.pos);                       // average position for cohesion
+        sep.add(PVector.sub(pos, o.pos).div(d * d)); // stronger repulsion when closer
         total++;
       }
     }
@@ -66,6 +66,7 @@ class Boid {
   }
 
   void wrap() {
+    // wrap edges so boids appear on opposite side when crossing boundaries
     if (pos.x < 0) pos.x = width;
     if (pos.x > width) pos.x = 0;
     if (pos.y < 0) pos.y = height;
@@ -73,12 +74,12 @@ class Boid {
   }
 
   void display() {
-    // Draw a black triangle pointing in velocity direction
+    // draw triangle pointing direction of velocity
     float a = vel.heading() + HALF_PI;
     pushMatrix();
     translate(pos.x, pos.y);
     rotate(a);
-    fill(0);       // black fill
+    fill(0);
     noStroke();
     beginShape();
     vertex(0, -6);
